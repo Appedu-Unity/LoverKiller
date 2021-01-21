7d49441e23551041f557684bc68f5456b4e3ac45
Chris.Suo <SuoChenHua@gmail.com>
2021-01-21 23:10:15 +0800

    add player&fire script
    add player&fire script

---
+++ 45_2D_GAME/Assets/Scripts/CameraController.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    [Header("Target")]
+    public Transform target;
+
+    [Header("Speed")]
+    public float speed = 3f;
+        
+    private void LateUpdate()
+    {
+        Track();
+    }
+    private void Track()
+    {
+        Vector3 posCurr = transform.position;
+        Vector3 posTarget = target.position;
+
+        posTarget.z = -10;
+
+        posCurr = Vector3.Lerp(posCurr, posTarget, speed * Time.deltaTime);
+        transform.position = posCurr;
+    }
+}
